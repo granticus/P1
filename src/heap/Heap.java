88f2071807fb83ff123e1b3e464fa064b9e26f6f
Grant Golden <grant.golden5@gmail.com>
2015-09-11 17:05:07 -0400

    File /C:/Users/grant_000/workspace/P1/src/heap/Heap.java changed at Fri Sep 11 17:05:06 EDT 2015

---
+++ src/heap/Heap.java
@@ -0,0 +1,19 @@
+package heap;
+
+public class Heap {
+	private int[] heapArray;
+	private int currentSize;
+	private int maxSize;
+	
+	public Heap (int max){
+		maxSize = max;
+		currentSize = 0;
+		heapArray = new int[maxSize];
+	}
+	
+	public int heapSize() { return currentSize; }
+	
+	public boolean isLeaf (int pos){
+		return (pos >= currentSize/2) && (pos < currentSize); 
+	}
+}
