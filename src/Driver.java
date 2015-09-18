06ba46ee2dcdd8a18f882c69457b15bb12f58917
Grant Golden <grant.golden5@gmail.com>
2015-09-18 18:10:35 -0400

    File /C:/Users/grant_000/workspace/P1/src/Driver.java changed at Fri Sep 18 18:10:35 EDT 2015

--- src/Driver.java
+++ src/Driver.java
@@ -8,11 +8,7 @@
  * @author Alex Hsu
  *
  */
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Driver {
 
@@ -24,42 +20,25 @@ public class Driver {
 		//This pretty much generates nS # of output files
 		File iFile = new File(args[1]);
 		PrintWriter oFile = new PrintWriter (args[2]);
+		InputStream read = new FileInputStream(iFile);
+		String[] lines = read.toString().split("\n");
 		
 		//FIRST LINE
-		int numSpecies = 3;
-		int numReactions = 3;
-		int numOutputted = 2;
-		int finalSimTime = 10;
-		
-		int[] populations = new int[numSpecies];
+		int[] fline = parse.getInts(lines[0]);
+		int numSpecies = fline[0];
+		int totalReactions = fline[1];
+		int numOutputted = fline[2];
+		int finalSimTime = fline[3];
 		
 		//SECOND LINE
-		for(int i = 0; i < numSpecies; i++){
-			//populations[i] = 	READ IN THE VALUES
-		}
-		populations[0] = 1000;
-		populations[1] = 1000;
-		populations[2] = 0; 	//FOR NOW, manually set them
-		
-		
-		//example parser
-		InputStream read = new FileInputStream(iFile);
-		String [] lines = read.toString().split("\n");
-		int[] fline = parse.getInts(lines[0]);
+		int[] populations = parse.getInts(lines[1]);
 			
 		//THIRD LINE
-		int[] trackedIndices = new int[numOutputted];
-
-		
-		for(int i = 0; i < numOutputted; i++){
-			//trackedIndices[i] = READ IN VALUES
-		}
-		trackedIndices[0] = 1;
-		trackedIndices[1] = 2;
+		int[] trackedIndices = parse.getInts(lines[2]);
 		
 		//REST OF LINES
-		Reaction [] reactions = new Reaction[numReactions];
-		for(int reactionNum = 0; reactionNum < numReactions; reactionNum++){
+		Reaction [] reactions = new Reaction[totalReactions];
+		for(int reactionNum = 0; reactionNum < totalReactions; reactionNum++){
 			//double k = .1; //READ IN K, DELETE THIS LINE
 			//reactions[reactionNum] = new Reaction(k, REACTANTS_ARRAY, NET_CHANGES);
 		}
@@ -72,7 +51,7 @@ public class Driver {
 		reactions[2] = reac3;
 		
 		//SETUP UP INITIAL PROPENSITIES
-		for(int i = 0; i < numReactions; i++){
+		for(int i = 0; i < totalReactions; i++){
 			reactions[i].calculatePropensity(populations);
 		}
 		
@@ -84,7 +63,7 @@ public class Driver {
 			}
 		}
 	}
-
+	
 	private static double nTau(double propensity) {
 		return Math.log(1/Math.random())/propensity;
 	}
