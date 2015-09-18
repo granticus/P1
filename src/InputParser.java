420bd6c414ba7f290ec33a1c60ccc9831448d140
Alex S Hsu <halex@vt.edu>
2015-09-17 21:56:21 -0400

    File /C:/Users/User/git/P1/src/InputParser.java changed at Thu Sep 17 21:56:21 EDT 2015

--- src/InputParser.java
+++ src/InputParser.java
@@ -48,6 +48,8 @@ public class InputParser {
 			int numSpecies = 1;
 			int species = 0;
 			int sIndex = elements[i].indexOf('S');
+			if (elements[i+1].substring(0, 1).equals("+"))
+				i++;
 			
 			if (elements[i].substring(0, 2).equals("->")) {
 				side = 1;
