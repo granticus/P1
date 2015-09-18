26bb75e2e39b83a873b337c652e0ee8d725f3529
Grant Golden <grant.golden5@gmail.com>
2015-09-17 21:08:37 -0400

    File /C:/Users/grant_000/workspace/P1/src/Reaction.java changed at Thu Sep 17 21:08:37 EDT 2015

--- src/Reaction.java
+++ src/Reaction.java
@@ -11,4 +11,16 @@ public class Reaction {
 		this.netChanges = netChanges;
 		this.k = k;
 	}
+	
+	public void calculatePropensity(int[] populations){
+		propensity = k;
+		
+		for(int i = 0; i < reactants.length; i++){
+			if(reactants[i] > 0){ //we have a reactant at i, and the value is the amount
+				for(int count = 0; count < reactants[i]; count++){
+					propensity *= populations[i] - count;
+				}
+			}
+		}
+	}
 }
