ca109311ca69c0e67653bb772f65df8671e02b5c
Alex S Hsu <halex@vt.edu>
2015-09-17 22:05:53 -0400

    File /C:/Users/User/git/P1/src/InputParser.java changed at Thu Sep 17 22:05:53 EDT 2015

--- src/InputParser.java
+++ src/InputParser.java
@@ -65,13 +65,13 @@ public class InputParser {
 			int numSpecies = 1;
 			int species = 0;
 			int sIndex = elements[i].indexOf('S');
-			
-			if (!elements[i].substring(0, 1).equals("+")) {
+			if (elements[i].substring(0, 2).equals("->")) {
+				side = 1;
+				rTable[kIndex][equationNumber] = Integer
+						.valueOf(elements[i].substring(3));
+			}
+			else if (!elements[i].substring(0, 1).equals("+")) {
 
-				if (elements[i].substring(0, 2).equals("->")) {
-					side = 1;
-					rTable[kIndex][equationNumber] = Integer
-							.valueOf(elements[i].substring(3));
 
 				} else if (elements[i + 1].substring(0, 1).equals("+")) {
 
