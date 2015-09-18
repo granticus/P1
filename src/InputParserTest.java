4c6cd449231fbd06f9ba8b41f44f5bfadc44b65d
Alex S Hsu <halex@vt.edu>
2015-09-18 18:54:40 -0400

    File /C:/Users/User/git/P1/src/InputParserTest.java changed at Fri Sep 18 18:54:40 EDT 2015

--- src/InputParserTest.java
+++ src/InputParserTest.java
@@ -8,7 +8,8 @@ import org.junit.Test;
  */
 
 /**
- * @author User
+ * @author Alex
+ * @author Grant
  *
  */
 public class InputParserTest {
@@ -22,8 +23,17 @@ public class InputParserTest {
 	}
 
 	@Test
-	public void test() {
+	public void test1() {
 		fail("Not yet implemented");
+		
+		String line1 = "3 4 1 10";
+		String line2 = "1000 200 0";
+		String line3 = "2";
+		String line4 = "-> 100 S1";
+		String line5 = "2S1 ->0.001 S2";
+		String line6 = "S2 ->0.5 2S1";
+		String line7 = "S2 ->0.04 S3";
+
 	}
 
 }
