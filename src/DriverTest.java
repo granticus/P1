f2d1218a83bcc46543ee6f3014f9a93558f4d965
Alex S Hsu <halex@vt.edu>
2015-09-19 18:52:33 -0400

    File /C:/Users/User/git/P1/src/DriverTest.java changed at Sat Sep 19 18:52:33 EDT 2015

---
+++ src/DriverTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DriverTest {
+
+	@Test
+	public void testMain() {
+		
+		String testFile = "3 4 3 10\n1000 200 0\n"
+				+ "1 2 3\n"
+				+ "->100 S1\n"
+				+ "2S1 ->0.001 S2\n"
+				+ "S2 ->0.5 2S1\n"
+				+ "S2 ->0.04 S3";
+		
+		
+	}
+
+}
