46a36a39f73e2c2b4d26f5aa57196c3a7f9a5127
Alex S Hsu <halex@vt.edu>
2015-09-20 15:39:06 -0400

    File /C:/Users/User/git/P1/src/heap/Heap.java changed at Sun Sep 20 15:39:06 EDT 2015

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -141,13 +141,11 @@ public class Heap
     public String print()
     {
         String heap = "";
-    	/*for (int i = 1; i <= size / 2; i++ )
+    	for (int i = 1; i <= size / 2; i++ )
         {
             heap = " PARENT : " + Heap[i] + " LEFT CHILD : " + Heap[2*i] 
                 + " RIGHT CHILD :" + Heap[2 * i  + 1] + "\n";
-        }*/
-        
-        
+        }
     	return heap;
     }
  
