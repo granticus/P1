2aa73190d44c0f2558e22a0543442140962c4b64
Alex S Hsu <halex@vt.edu>
2015-09-21 17:34:45 -0400

    File /C:/Users/User/git/P1/src/DependencyTableTest.java changed at Mon Sep 21 17:34:45 EDT 2015

--- src/DependencyTable.java
+++ src/DependencyTable.java
@@ -1,3 +1,5 @@
+
+
 public class DependencyTable {
 
 	private Reaction[] table;
--- src/DependencyTableTest.java
+++ src/DependencyTableTest.java
@@ -9,7 +9,14 @@ public class DependencyTableTest {
 	private DependencyTable tTable;
 	@Before
 	public void setUp() throws Exception {
-		tTable = new DependencyTable(null, 5);
+		Reaction [] reactions = new Reaction[3];
+		Reaction reac1 = new Reaction(10, new int[]{1, 0, 0}, new int[]{1, 0, 0});
+		Reaction reac2 = new Reaction(.01, new int[]{1, 1, 0}, new int[]{-1, 1, 0});
+		Reaction reac3 = new Reaction(10, new int[]{0, 1, 0}, new int[]{0, -1, 1}); // FOR NOW, manually set
+		reactions[0] = reac1;
+		reactions[1] = reac2;
+		reactions[2] = reac3;
+		tTable = new DependencyTable(reactions, 5);
 	}
 
 	@Test
