a5e6b8cccf7fd2113af4bc79a7bc4cd6c99aa3db
Alex S Hsu <halex@vt.edu>
2015-09-21 15:19:44 -0400

    File /C:/Users/User/git/P1/src/DependencyTable.java changed at Mon Sep 21 15:19:44 EDT 2015

--- src/DependencyTable.java
+++ src/DependencyTable.java
@@ -1,30 +1,28 @@
 
-
 public class DependencyTable {
-	
-	private Reaction [] table;
+
+	private Reaction[] table;
 	private int[][] indexReaction;
-	
+
 	public DependencyTable(Reaction[] reactionArray) {
-		//TODO later
-		
+		// TODO later
+
 		table = reactionArray;
 	}
-	
-	public Reaction[] updatePropensity(int [] reactants) {
-		
-		
-		
+
+	public Reaction[] updatePropensity(int[] reactants) {
+
 		return table;
 	}
-	
-	public void setUpTable() {
-		for (Reaction reac: table) {
-			
+
+	public void setUpTable(int totSpecies) {
+		for (int spec = 0; spec < totSpecies; spec++) {
+
+			for (int i = 0; i < table.length; i++) {
+				int [spec][i];
+			}
+
 		}
-		
 	}
-	
-	
 
 }
