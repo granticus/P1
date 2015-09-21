9f5099630f40365167ed6a7250091b5ad52a13af
Alex S Hsu <halex@vt.edu>
2015-09-21 14:38:13 -0400

    File /C:/Users/User/git/P1/src/Heap.java changed at Mon Sep 21 14:38:13 EDT 2015

--- src/Heap.java
+++ src/Heap.java
@@ -1,183 +1,178 @@
 
-
-
-
 /**
- * The Class Heap that will store the minimum value at the top and every child will be a larger value than the parent
+ * The Class Heap that will store the minimum value at the top and every child
+ * will be a larger value than the parent
  */
-public class Heap
-{
-    
-    /** The Heap. An array that will store all the values. */
-    private Reaction[] Heap;
-    
-    /** The size. */
-    private int size;
-    
-    /** The maxsize. */
-    private int maxsize;
- 
-    /** The Constant FRONT. */
-    private static final int FRONT = 1;
- 
-    /**
-     * Instantiates a new heap.
-     *
-     * @param maxsize the maxsize
-     */
-    public Heap(int maxsize)
-    {
-        this.maxsize = maxsize;
-        this.size = 0;
-        Heap = new Reaction[this.maxsize + 1];
-        Heap[0] = new NullReaction();
-    }
- 
-    /**
-     * The index of the Parent.
-     *
-     * @param pos the pos that is currently the child
-     * @return the int the index of the parent
-     */
-    private int parent(int pos)
-    {
-        return pos / 2;
-    }
- 
-    /**
-     * Left child.
-     *
-     * @param pos the pos
-     * @return the int
-     */
-    private int leftChild(int pos)
-    {
-        return (2 * pos);
-    }
- 
-    /**
-     * Right child.
-     *
-     * @param pos the pos
-     * @return the int
-     */
-    private int rightChild(int pos)
-    {
-        return (2 * pos) + 1;
-    }
- 
-    /**
-     * Checks if is leaf.
-     *
-     * @param pos the pos
-     * @return true, if is leaf
-     */
-    private boolean isLeaf(int pos)
-    {
-        if (pos >=  (size / 2)  &&  pos <= size)
-        { 
-            return true;
-        }
-        return false;
-    }
- 
-    /**
-     * Swap the position of the 2 taken elements in the array.
-     *
-     * @param fpos the fpos
-     * @param spos the spos
-     */
-    private void swap(int fpos, int spos)
-    {
-        Reaction tmp;
-        tmp = Heap[fpos];
-        Heap[fpos] = Heap[spos];
-        Heap[spos] = tmp;
-    }
- 
-    /**
-     * Min heapify.
-     *
-     * @param pos the pos
-     */
-    private void minHeapify(int pos)
-    {
-        if (!isLeaf(pos))
-        { 
-            if ( Heap[pos].compareTo(Heap[leftChild(pos)]) > 0 || Heap[pos].compareTo(Heap[rightChild(pos)]) > 0)
-            		//Heap[pos] > Heap[leftChild(pos)]  || Heap[pos] > Heap[rightChild(pos)])
-            {
-                if (Heap[leftChild(pos)].compareTo(Heap[rightChild(pos)]) < 0)
-                		//Heap[leftChild(pos)] < Heap[rightChild(pos)])
-                {
-                    swap(pos, leftChild(pos));
-                    minHeapify(leftChild(pos));
-                }else
-                {
-                    swap(pos, rightChild(pos));
-                    minHeapify(rightChild(pos));
-                }
-            }
-        }
-    }
- 
-    /**
-     * Inserts and elements into a valid position inside the heap.
-     *
-     * @param element the element that will be inserted inside the heap.
-     */
-    public void insert(Reaction element)
-    {
-        Heap[++size] = element;
-        int current = size;
- 
-        while (Heap[current].compareTo(Heap[parent(current)])<0)
-        		//Heap[current] < Heap[parent(current)])
-        {
-            swap(current,parent(current));
-            current = parent(current);
-        }	
-    }
- 
-    /**
-     * Prints the Heap.
-     */
-    public String print()
-    {
-        String heap = "";
-    	/*for (int i = 1; i <= size / 2; i++ )
-        {
-            heap = " PARENT : " + Heap[i] + " LEFT CHILD : " + Heap[2*i] 
-                + " RIGHT CHILD :" + Heap[2 * i  + 1] + "\n";
-        }*/
-        
-        for (int i = 0; i < Heap.length; i++) {
-        	heap += Heap[i] + " ";
-        }
-    	return heap;
-    }
- 
-    /**
-     * Min heap.
-     */
-    public void minHeap()
-    {
-        for (int pos = (size / 2); pos >= 1 ; pos--)
-        {
-            minHeapify(pos);
-        }
-    }
- 
-    /**
-     * Removes the.
-     *
-     * @return the int
-     */
-    public Reaction remove()
-    {
-        Reaction popped = Heap[FRONT];
-        Heap[FRONT] = Heap[size--]; 
-        minHeapify(FRONT);
-        return popped;
-    }
+public class Heap {
+
+	/** The Heap. An array that will store all the values. */
+	private Reaction[] Heap;
+
+	/** The size. */
+	private int size;
+
+	/** The maxsize. */
+	private int maxsize;
+
+	/** The Constant FRONT. */
+	private static final int FRONT = 1;
+
+	/**
+	 * Instantiates a new heap.
+	 *
+	 * @param maxsize
+	 *            the maxsize
+	 */
+	public Heap(int maxsize) {
+		this.maxsize = maxsize;
+		this.size = 0;
+		Heap = new Reaction[this.maxsize + 1];
+		Heap[0] = new NullReaction();
+	}
+
+	/**
+	 * The index of the Parent.
+	 *
+	 * @param pos
+	 *            the pos that is currently the child
+	 * @return the int the index of the parent
+	 */
+	private int parent(int pos) {
+		return pos / 2;
+	}
+
+	/**
+	 * Left child.
+	 *
+	 * @param pos
+	 *            the pos
+	 * @return the int
+	 */
+	private int leftChild(int pos) {
+		return (2 * pos);
+	}
+
+	/**
+	 * Right child.
+	 *
+	 * @param pos
+	 *            the pos
+	 * @return the int
+	 */
+	private int rightChild(int pos) {
+		return (2 * pos) + 1;
+	}
+
+	/**
+	 * Checks if is leaf.
+	 *
+	 * @param pos
+	 *            the pos
+	 * @return true, if is leaf
+	 */
+	private boolean isLeaf(int pos) {
+		if (pos >= (size / 2) && pos <= size) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Swap the position of the 2 taken elements in the array.
+	 *
+	 * @param fpos
+	 *            the fpos
+	 * @param spos
+	 *            the spos
+	 */
+	private void swap(int fpos, int spos) {
+		Reaction tmp;
+		tmp = Heap[fpos];
+		Heap[fpos] = Heap[spos];
+		Heap[spos] = tmp;
+	}
+
+	/**
+	 * Takes the parent index, and first makes sure the element is not a leaf,
+	 * then makes sure the children of the index are bigger than the parent.
+	 * Also, calls itself recursively to sort the whole tree.
+	 *
+	 * @param pos
+	 *            the pos
+	 */
+	private void minHeapify(int pos) {
+		if (!isLeaf(pos)) {
+			if (Heap[pos].compareTo(Heap[leftChild(pos)]) > 0
+					|| Heap[pos].compareTo(Heap[rightChild(pos)]) > 0)
+			// Heap[pos] > Heap[leftChild(pos)] || Heap[pos] >
+			// Heap[rightChild(pos)])
+			{
+				if (Heap[leftChild(pos)].compareTo(Heap[rightChild(pos)]) < 0)
+				// Heap[leftChild(pos)] < Heap[rightChild(pos)])
+				{
+					swap(pos, leftChild(pos));
+					minHeapify(leftChild(pos));
+				} else {
+					swap(pos, rightChild(pos));
+					minHeapify(rightChild(pos));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Inserts and elements into a valid position inside the heap.
+	 *
+	 * @param element
+	 *            the element that will be inserted inside the heap.
+	 */
+	public void insert(Reaction element) {
+		Heap[++size] = element;
+		int current = size;
+
+		while (Heap[current].compareTo(Heap[parent(current)]) < 0)
+		// Heap[current] < Heap[parent(current)])
+		{
+			swap(current, parent(current));
+			current = parent(current);
+		}
+	}
+
+	/**
+	 * Prints the Heap.
+	 */
+	public String print() {
+		String heap = "";
+		/*
+		 * for (int i = 1; i <= size / 2; i++ ) { heap = " PARENT : " + Heap[i]
+		 * + " LEFT CHILD : " + Heap[2*i] + " RIGHT CHILD :" + Heap[2 * i + 1] +
+		 * "\n"; }
+		 */
+
+		for (int i = 0; i < Heap.length; i++) {
+			heap += Heap[i] + " ";
+		}
+		return heap;
+	}
+
+	/**
+	 * Min heap.
+	 */
+	public void minHeap() {
+		for (int pos = (size / 2); pos >= 1; pos--) {
+			minHeapify(pos);
+		}
+	}
+
+	/**
+	 * Removes the.
+	 *
+	 * @return the int
+	 */
+	public Reaction remove() {
+		Reaction popped = Heap[FRONT];
+		Heap[FRONT] = Heap[size--];
+		minHeapify(FRONT);
+		return popped;
+	}
 }
