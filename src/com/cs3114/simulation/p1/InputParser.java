d3301b7992d79267a47b519f668915edb11ea62f
Alex S Hsu <halex@vt.edu>
2015-09-21 19:54:00 -0400

    File /C:/Users/User/git/P1/src/com/cs3114/simulation/p1/InputParser.java changed at Mon Sep 21 19:54:00 EDT 2015

--- src/com/cs3114/simulation/p1/InputParser.java
+++ src/com/cs3114/simulation/p1/InputParser.java
@@ -125,7 +125,7 @@ public class InputParser {
 			if (endIndex == -1) {
 				endIndex = newString.length();
 			}
-			species = Integer.valueOf(elements[i].substring(sIndex + 1)) - firstIndex;
+			species = Integer.valueOf(elements[i].substring(sIndex + 1, endIndex)) - firstIndex;
 			// careful,sometimes the reaction starts at zero.
 
 			reactants[species] += numSpecies;
