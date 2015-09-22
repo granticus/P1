1996edd25e7743299482e625d61fd8f2efa3d302
Alex S Hsu <halex@vt.edu>
2015-09-22 00:53:20 -0400

    File /C:/Users/User/git/P1/src/com/cs3114/simulation/p1/HeapTest.java changed at Tue Sep 22 00:53:20 EDT 2015

--- src/com/cs3114/simulation/p1/HeapTest.java
+++ src/com/cs3114/simulation/p1/HeapTest.java
@@ -31,20 +31,10 @@ public class HeapTest {
 	@Test
 	public void test() {
 		
-        /*testHeap.insert(5);
-        testHeap.insert(3);
-        testHeap.insert(17);
-        testHeap.insert(10);
-        testHeap.insert(84);
-        testHeap.insert(19);
-        testHeap.insert(6);
-        testHeap.insert(22);
-        testHeap.insert(9);*/
         testHeap.minHeap();
  
         System.out.print(testHeap.print());
         
-        //System.out.println("The Min val is " + testHeap.remove());
         
         assertTrue(true);
         
