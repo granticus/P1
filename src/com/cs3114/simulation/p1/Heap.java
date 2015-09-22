73e65a8e84c03becba1c0bb087eff6c0c597e76a
Alex S Hsu <halex@vt.edu>
2015-09-22 00:24:01 -0400

    File /C:/Users/User/git/P1/src/com/cs3114/simulation/p1/Heap.java changed at Tue Sep 22 00:24:01 EDT 2015

--- src/com/cs3114/simulation/p1/Heap.java
+++ src/com/cs3114/simulation/p1/Heap.java
@@ -22,9 +22,11 @@ public class Heap {
 	 */
 	public Heap(Reaction [] mainReaction) {
 		this.maxsize = mainReaction.length +1;
-		this.size = 4 + 1;
-		Heap = mainReaction;
+		this.size = mainReaction.length;
 		Heap[0] = new NullReaction();
+		for (int i = 0; i < mainReaction.length; i++) {
+			Heap[i+1] = mainReaction[i]; 
+		}
 	}
 
 	/**
