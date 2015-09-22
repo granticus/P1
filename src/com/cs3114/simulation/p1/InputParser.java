d98a01e4dd316fc1016622bf79f9226b34995f94
Alex S Hsu <halex@vt.edu>
2015-09-21 20:18:51 -0400

    File /C:/Users/User/git/P1/src/com/cs3114/simulation/p1/InputParser.java changed at Mon Sep 21 20:18:51 EDT 2015

--- src/com/cs3114/simulation/p1/InputParser.java
+++ src/com/cs3114/simulation/p1/InputParser.java
@@ -136,7 +136,7 @@ public class InputParser {
 		
 		String[] elements = line.split(" ");
 		int[] reactants = new int[totalSpecies];
-		
+		if (elements[0].substring(0, 2).equals("->"))
 		return reactants;
 	}
 
