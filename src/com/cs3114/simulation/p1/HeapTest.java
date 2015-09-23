746cb166e3be2d9eb82a25224e183a49e3d1aef1
Alex S Hsu <halex@vt.edu>
2015-09-22 21:43:53 -0400

    File /C:/Users/User/git/P1/src/com/cs3114/simulation/p1/HeapTest.java changed at Tue Sep 22 21:43:53 EDT 2015

--- src/com/cs3114/simulation/p1/HeapTest.java
+++ src/com/cs3114/simulation/p1/HeapTest.java
@@ -1,6 +1,5 @@
 package com.cs3114.simulation.p1;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -9,55 +8,58 @@ import org.junit.Test;
 public class HeapTest {
 
 	Heap testHeap;
+
 	@Before
 	public void setUp() throws Exception {
-	
-		Reaction [] reactions = new Reaction[4];
+
+		Reaction[] reactions = new Reaction[4];
 		int counter = 0;
-		Reaction reac1 = new Reaction(10, new int[]{1, 0, 0}, new int[]{1, 0, 0}, counter++);
+		Reaction reac1 = new Reaction(10, new int[] { 1, 0, 0 }, new int[] { 1,
+				0, 0 }, counter++);
 		reac1.setCurrentTau(.5);
-		Reaction reac2 = new Reaction(.01, new int[]{1, 1, 0}, new int[]{-1, 1, 0}, counter++);
+		Reaction reac2 = new Reaction(.01, new int[] { 1, 1, 0 }, new int[] {
+				-1, 1, 0 }, counter++);
 		reac2.setCurrentTau(.3);
-		Reaction reac3 = new Reaction(10, new int[]{0, 1, 0}, new int[]{0, -1, 1}, counter++);
+		Reaction reac3 = new Reaction(10, new int[] { 0, 1, 0 }, new int[] { 0,
+				-1, 1 }, counter++);
 		reac3.setCurrentTau(.9);
-		Reaction reac4 = new Reaction(25, new int[]{0, 0, 1}, new int[]{0, 1, -1}, counter++);
+		Reaction reac4 = new Reaction(25, new int[] { 0, 0, 1 }, new int[] { 0,
+				1, -1 }, counter++);
 		reac4.setCurrentTau(.6);
-		
+
 		reactions[0] = reac1;
 		reactions[1] = reac2;
 		reactions[2] = reac3;
 		reactions[3] = reac4;
-		
+
 		testHeap = new Heap(reactions);
-		
-	
+
 	}
 
 	@Test
 	public void testHeap() {
 
-        testHeap.minHeap();
-        Reaction testReaction = testHeap.remove();
-        assertTrue(.3 == testReaction.getCurrentTau());
-        testReaction.setCurrentTau(1.2);
-        testHeap.insert(testReaction);
-        
-        testReaction = testHeap.remove();
-        assertTrue(.5 == testReaction.getCurrentTau());
-        testReaction.setCurrentTau(1.5);
-        testHeap.insert(testReaction);
+		testHeap.minHeap();
+		Reaction testReaction = testHeap.remove();
+		assertTrue(.3 == testReaction.getCurrentTau());
+		testReaction.setCurrentTau(1.2);
+		testHeap.insert(testReaction);
 
-        testReaction = testHeap.remove();
-        assertTrue(.6 == testReaction.getCurrentTau());
-        testReaction.setCurrentTau(.8);
-        testHeap.insert(testReaction);
+		testReaction = testHeap.remove();
+		assertTrue(.5 == testReaction.getCurrentTau());
+		testReaction.setCurrentTau(1.5);
+		testHeap.insert(testReaction);
 
-        testReaction = testHeap.remove();
-        assertTrue(.8 == testReaction.getCurrentTau());
-        testReaction.setCurrentTau(2.0);
-        testHeap.insert(testReaction);
+		testReaction = testHeap.remove();
+		assertTrue(.6 == testReaction.getCurrentTau());
+		testReaction.setCurrentTau(.8);
+		testHeap.insert(testReaction);
 
-        
+		testReaction = testHeap.remove();
+		assertTrue(.8 == testReaction.getCurrentTau());
+		testReaction.setCurrentTau(2.0);
+		testHeap.insert(testReaction);
+
 	}
 
 }
