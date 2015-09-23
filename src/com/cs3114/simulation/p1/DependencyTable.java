e4b9d35232a876a0d06ad3a28c765d4395891a26
Alex S Hsu <halex@vt.edu>
2015-09-22 20:35:09 -0400

    File /C:/Users/User/git/P1/src/com/cs3114/simulation/p1/DependencyTable.java changed at Tue Sep 22 20:35:09 EDT 2015

--- src/com/cs3114/simulation/p1/DependencyTable.java
+++ src/com/cs3114/simulation/p1/DependencyTable.java
@@ -5,11 +5,13 @@ import java.util.*;
 public class DependencyTable {
 
 	private Reaction[] table;
-	private boolean[][] reactionIndex;
+	private boolean[][] reactionIndex;	
+	private int [][] specReactions;
 	ArrayList<ArrayList<Reaction>>  rTable;
 
 	public DependencyTable(Reaction[] reactionArray) {
 		table = reactionArray;
+		specReactions = new int[reactionArray.length][reactionArray.length];
 		reactionIndex = new boolean[reactionArray.length][reactionArray.length];
 		rTable = new ArrayList<ArrayList<Reaction>>();
 		setUpTable();
@@ -26,11 +28,7 @@ public class DependencyTable {
 		}
 		curFired.calculatePropensity(populations);
 		
-		for (int c = 0; c < reactionIndex[0].length; c++) {
-			if (reactionIndex[currIndex][c] == true) {
-				table[c].calculatePropensity(populations);
-			}
-		}
+		
 
 		for (int k = 0; k < net.length; k++) {
 			populations[k] += net[k];
@@ -42,6 +40,10 @@ public class DependencyTable {
 
 		for(int i = 0; i < table.length; i++) {
 			rTable.add(new ArrayList<Reaction>());
+		}
+		
+		for (int i = 0; i < table.length; i++) {
+			specReactions[i] = table[i].getReactants();
 		}
 		
 		for (int i = 0; i < table.length; i++) {
@@ -65,6 +67,17 @@ public class DependencyTable {
 				}
 			}
 		}
+	}
+	
+	private boolean[] getDependencies(int species) {
+		boolean[] depen = new boolean[table.length];
+		for (int r = 0; r < specReactions.length; r++) {
+			if (specReactions[r][species] != 0) {
+				depen[r] = true;
+			}
+		}
+		
+		return depen;
 	}
 
 	public boolean[][] getIR() {
