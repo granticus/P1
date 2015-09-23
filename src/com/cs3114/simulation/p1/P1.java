ba0607f33f36c8dfe6d6eedf6d8fb20e6538dc3c
Alex S Hsu <halex@vt.edu>
2015-09-22 22:38:31 -0400

    Comment the classes I did (Heap, InputParser, DependencyTable)
    Update the P1 driver with the data structures.

--- src/com/cs3114/simulation/p1/P1.java
+++ src/com/cs3114/simulation/p1/P1.java
@@ -1,4 +1,5 @@
 package com.cs3114.simulation.p1;
+
 /**
  * Operating System: Windows 8.1
  * Compiler: Eclipse 
@@ -35,191 +36,202 @@ import java.io.*;
 public class P1 {
 
 	public static void main(String[] args) throws IOException {
-	
-		
-		//INPUTTED DATA
+
+		// INPUTTED DATA
 		InputParser parse = new InputParser();
-		
+
 		int numSimulations = Integer.parseInt(args[0]);
-		//This pretty much generates nS # of output files
+		// This pretty much generates nS # of output files
 		File oFile = new File(args[2]);
 		oFile.createNewFile();
-		BufferedWriter bw = new BufferedWriter(new FileWriter(oFile.getAbsoluteFile()));
-		
-		/*
-		File iFile = new File(args[1]);
-		InputStream read = new FileInputStream(iFile);
-		String[] lines = read.toString().split("\n");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(
+				oFile.getAbsoluteFile()));
 
-	*/
-		
+		/*
+		 * File iFile = new File(args[1]); InputStream read = new
+		 * FileInputStream(iFile); String[] lines = read.toString().split("\n");
+		 */
+
 		FileReader freader = new FileReader(args[1]);
 		BufferedReader bReader = new BufferedReader(freader);
-		//String[] lines = bReader.toString().split("\n");
+		// String[] lines = bReader.toString().split("\n");
 		String file = "";
 		String line = "";
-		while((line=bReader.readLine()) != null) {
+		while ((line = bReader.readLine()) != null) {
 			file += line + "\n";
 		}
-		String [] lines = file.split("\n");
-		
-		//gets where the indexing starts, either 0 (S0), or 1 (S1)
+		String[] lines = file.split("\n");
+
+		// gets where the indexing starts, either 0 (S0), or 1 (S1)
 		int offset = parse.getFirstIndex(lines[3]);
-		
-		//FIRST LINE
+
+		// FIRST LINE
 		int[] fline = parse.getInts(lines[0]);
-		//total number of species
+		// total number of species
 		int numSpecies = fline[0];
-		//total number of reactions
+		// total number of reactions
 		int totalReactions = fline[1];
-		//total number of outputted
+		// total number of outputted
 		int numOutputted = fline[2];
-		//the final simulation time
+		// the final simulation time
 		int finalSimTime = fline[3];
-		
-		//SECOND LINE
-		//stores the current populations for each species
+
+		// SECOND LINE
+		// stores the current populations for each species
 		int[] populations;
 
-		//THIRD LINE
-		//stores the array of indices to be tracked
+		// THIRD LINE
+		// stores the array of indices to be tracked
 		int[] trackedIndices = parse.getInts(lines[2]);
-		//this loop updates 
-		for(int i = 0; i < numOutputted; i++){
+		// this loop updates
+		for (int i = 0; i < numOutputted; i++) {
 			trackedIndices[i] = trackedIndices[i] - offset;
 		}
 		int counter = 0;
-		//REST OF LINES
-		Reaction [] reactions = new Reaction[totalReactions];
-		for(int reactionNum = 0; reactionNum < totalReactions; reactionNum++){
-			reactions[reactionNum] = new Reaction(parse.getKConstant(lines[reactionNum+3]), parse.getReactants(lines[reactionNum+3], numSpecies),
-					parse.getEquation(lines[reactionNum+3], numSpecies), counter++);
+		// REST OF LINES
+		Reaction[] reactions = new Reaction[totalReactions];
+		for (int reactionNum = 0; reactionNum < totalReactions; reactionNum++) {
+			reactions[reactionNum] = new Reaction(
+					parse.getKConstant(lines[reactionNum + 3]),
+					parse.getReactants(lines[reactionNum + 3], numSpecies),
+					parse.getEquation(lines[reactionNum + 3], numSpecies),
+					counter++);
 		}
-		
-		int [][] finalPops = new int[numSimulations][numSpecies];
-		
+
+		int[][] finalPops = new int[numSimulations][numSpecies];
+
 		double currentTime;
-		
+
 		Heap reactionHeap;
 		DependencyTable dependency = new DependencyTable(reactions);
 		ArrayList<Reaction> dependents;
-		
-		for(int i = 0; i < numSimulations; i++){
-			
+
+		for (int i = 0; i < numSimulations; i++) {
+
 			currentTime = 0;
 			populations = parse.getInts(lines[1]);
 
-			for(int j = 0; j < totalReactions; j++){
-				reactions[j].setCurrentTau(nTau(reactions[j].calculatePropensity(populations)));
+			for (int j = 0; j < totalReactions; j++) {
+				reactions[j].setCurrentTau(nTau(reactions[j]
+						.calculatePropensity(populations)));
 			}
 
 			reactionHeap = new Heap(reactions);
-			
-			while(currentTime < finalSimTime){
-				
-				if(numSimulations == 1){
+
+			while (currentTime < finalSimTime) {
+
+				if (numSimulations == 1) {
 					String newLine = currentTime + "\t";
-					for(int j = 0; j < numOutputted; j++){
+					for (int j = 0; j < numOutputted; j++) {
 						newLine += populations[trackedIndices[j]];
-						if(j != numOutputted - 1){ newLine += "\t"; }
-						else { newLine += "\n"; }
+						if (j != numOutputted - 1) {
+							newLine += "\t";
+						} else {
+							newLine += "\n";
+						}
 					}
-					
+
 					bw.write(newLine);
 				}
-				
-				//choose lowest fire time
-				//Gets the lowest firing time from the heap.
+
+				// choose lowest fire time
+				// Gets the lowest firing time from the heap.
 				Reaction minReaction = reactionHeap.minElement();
-				
-				//Gets the arraylist of dependents from the current reaction fired.
+
+				// Gets the arraylist of dependents from the current reaction
+				// fired.
 				dependents = dependency.getDependents(minReaction);
 				minReaction.calculatePropensity(populations);
-				
+
 				reactionHeap.minHeap();
-				
-				//update populations using the netChange of the chosen reaction
+
+				// update populations using the netChange of the chosen reaction
 				int[] currNetChange = minReaction.getNetChanges();
-				
+
 				minReaction.incrementFired();
-				
-				for(int j = 0; j < numSpecies; j++){
+
+				for (int j = 0; j < numSpecies; j++) {
 					populations[j] += currNetChange[j];
 				}
-				
-				/**/
+
+				/*
+				 * Once the population has been updated, we need to update the
+				 * propensities that have been changed because of the reaction
+				 * that was just fired
+				 */
 				for (int dIndex = 0; dIndex < dependents.size(); dIndex++) {
 					dependents.get(dIndex).calculatePropensity(populations);
 				}
-				
-				//add chosen time to currentTime
+
+				// add chosen time to currentTime
 				currentTime += minReaction.getCurrentTau();
 			}
-			for(int j = 0; j < numSpecies; j++){
+			for (int j = 0; j < numSpecies; j++) {
 				finalPops[i][j] = populations[j];
 			}
 		}
 
-		if (numSimulations == 1){
-			for(int i = 0; i < totalReactions; i++){
+		if (numSimulations == 1) {
+			for (int i = 0; i < totalReactions; i++) {
 				bw.write(reactions[i].getNumFired() + "\n");
 			}
-		}else { // more than 1 sim
+		} else { // more than 1 sim
 			double[] sum = new double[numSpecies];
 			double[] mean = new double[numSpecies];
-			
-			for(int i = 0; i < numSimulations; i++){
-				for(int j = 0; j < numSpecies; j++){
+
+			for (int i = 0; i < numSimulations; i++) {
+				for (int j = 0; j < numSpecies; j++) {
 					sum[j] += finalPops[i][j];
 				}
 			}
-			
-			for(int i = 0; i < numSpecies; i++){
-				mean[i] = sum[i]/numSimulations;
+
+			for (int i = 0; i < numSpecies; i++) {
+				mean[i] = sum[i] / numSimulations;
 			}
-			
+
 			double[] variances = new double[numSpecies];
-			
-			for(int i = 0; i < numSimulations; i++){
-				for(int j = 0; j < numSpecies; j++){
-					variances[j] += (finalPops[i][j] - mean[j])*(finalPops[i][j] - mean[j]);
+
+			for (int i = 0; i < numSimulations; i++) {
+				for (int j = 0; j < numSpecies; j++) {
+					variances[j] += (finalPops[i][j] - mean[j])
+							* (finalPops[i][j] - mean[j]);
 				}
 			}
-			
-			for(int i = 0; i < numSpecies; i++){
+
+			for (int i = 0; i < numSpecies; i++) {
 				variances[i] /= (numSimulations - 1);
 			}
-			
-			//STANDARD DEVIATION
-			double []stdPops = new double[numSpecies];
-			for(int i = 0; i < numSpecies; i++){
+
+			// STANDARD DEVIATION
+			double[] stdPops = new double[numSpecies];
+			for (int i = 0; i < numSpecies; i++) {
 				stdPops[i] = Math.sqrt(variances[i]);
 			}
-			
-			//OUTPUT THIS STUFF
-			
-			for(int i = 0; i < numSimulations; i++){
-				for(int j = 0; j < numOutputted; j++){
+
+			// OUTPUT THIS STUFF
+
+			for (int i = 0; i < numSimulations; i++) {
+				for (int j = 0; j < numOutputted; j++) {
 					bw.write("" + finalPops[i][j]);
-					if(j != numOutputted - 1){
+					if (j != numOutputted - 1) {
 						bw.write("\t");
-					}else{
+					} else {
 						bw.write("\n");
 					}
 				}
 			}
-			
+
 			String meanString = "";
-			for(int i = 0; i < numOutputted; i++){
+			for (int i = 0; i < numOutputted; i++) {
 				int index = trackedIndices[i];
 				meanString += mean[index] + "\t";
 			}
 			meanString += "\n";
 			System.out.print(meanString);
 			bw.write(meanString);
-			
+
 			String varString = "";
-			for(int i = 0; i < numOutputted; i++){
+			for (int i = 0; i < numOutputted; i++) {
 				int index = trackedIndices[i];
 				varString += variances[index] + "\t";
 			}
@@ -227,13 +239,13 @@ public class P1 {
 			System.out.print(varString);
 			bw.write(varString);
 		}
-		
+
 		bw.close();
 		bReader.close();
-		
+
 	}
-	
+
 	private static double nTau(double propensity) {
-		return Math.log(1/Math.random())/propensity;
+		return Math.log(1 / Math.random()) / propensity;
 	}
 }
